be6e8fce084d49eac303bdee58238157df9e9588
mapadilha <devpadilha@gmail.com>
2022-08-30 21:16:09 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/domain/usecases/add-account/db-add-account-protocols.ts
+++ src/domain/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
 export * from '../../../presentation/protocols/encrypter'
+export * from '../../../presentation/protocols/add-account-repository'
